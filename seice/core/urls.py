46e9ea8849124272d2cfcf943ebe8add7fb9f03f
Pedro Henricky Oliveira Barbosa <vascodagama.barbosa@gmail.com>
2025-05-22 17:23:32 -0300

    Tela de login antes de entrar no sistema
    senha e usuario ja cadastrados

--- seice/core/urls.py
+++ seice/core/urls.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.index, name='index'),
+
+
+
     path('api/estagiarios/', views.get_estagiarios),
     path('api/estagiarios/create/', views.create_estagiario),
 
