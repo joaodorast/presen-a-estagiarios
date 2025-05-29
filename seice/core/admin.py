71dd1695a1fcd5ae69b52fdff214acaf8345303b
Pedro Henricky Oliveira Barbosa <vascodagama.barbosa@gmail.com>
2025-05-29 13:19:55 -0300

    Resolvi o problema do modal de area

--- seice/core/admin.py
+++ seice/core/admin.py
@@ -1,6 +1,7 @@
 # filepath: core/admin.py
 from django.contrib import admin
-from .models import Estagiario, Presenca
+from .models import Estagiario, Presenca, Area
 
 admin.site.register(Estagiario)
 admin.site.register(Presenca)
+admin.site.register(Area)
