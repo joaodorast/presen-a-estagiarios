4b9afefb111f66d1a2dd0cc65d578f597657ef6f
CkyHP <160540639+CkyHP@users.noreply.github.com>
2025-07-03 17:33:17 -0300

    Software para adicionar digitais

--- seice/seice/settings.py
+++ seice/seice/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',  # Adicionando corsheaders
     'core', 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Adicionando CorsMiddleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True  # Para testes locais
+# Ou 
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8080",
+#     "http://localhost:5500",
+# ]
 
 ROOT_URLCONF = 'seice.urls'
 
