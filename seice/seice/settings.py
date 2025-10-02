563ec8001736ab269d8fe98451c90f07d0dfc2e4
Pedro Henricky Oliveira Barbosa <160540639+CkyHP@users.noreply.github.com>
2025-10-02 15:04:03 -0300

    subi o banco de dados

--- seice/seice/settings.py
+++ seice/seice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rhwjv=&ttztbp5x4q6^!6)&tm@c=p%*_#5)&fo598d787pw!-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["192.168.2.174"]
 
 
 # Application definition
